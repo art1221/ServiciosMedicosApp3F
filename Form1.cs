9274cf60ac4b80798e6ee09d547690f1d2ef3c90
art1221 <arturo.rojas@utpuebla.edu.mx>
2026-07-14 13:38:12 -0600

    version 14 de julio

--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Contracts;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,36 @@ namespace ServiciosMedicosApp
         private void frmPrincipal_Load(object sender, EventArgs e)
         {
             lblBienvenida.Text = $"Bienvenido: {usuarioActual.Nombreusuario} ({usuarioActual.Rol})";
+
+            switch (usuarioActual.Rol)
+            {
+                case "admin":
+                    CargarPestaña(new frmEstudiantes(), "Estudiantes");
+                    CargarPestaña(new frmConsultas(), "Consultas Medicas");
+                    CargarPestaña(new frmJustificaciones(), "Justificaciones");
+                    break;
+                case "enfermera":
+                    CargarPestaña(new frmEstudiantes(), "Estudiantes");
+                    CargarPestaña(new frmConsultas(), "Consultas Medicas");
+                    break;
+                case "docente":
+                    CargarPestaña(new frmJustificaciones(), "Justificaciones");
+                    break;
+                case "estudiante":
+                    CargarPestaña(new frmHistorial(), "Mihistorial Medico");
+                    break;
+            }
+        }
+
+        private void CargarPestaña(Form formHijo, string titulo)
+        {
+            TabPage nuevaPestaña = new TabPage(titulo);
+            formHijo.TopLevel = false;
+            formHijo.FormBorderStyle = FormBorderStyle.None;
+            formHijo.Dock = DockStyle.Fill;
+            nuevaPestaña.Controls.Add(formHijo);
+            formHijo.Show();
+            tabControlPrincipal.TabPages.Add(nuevaPestaña);
         }
     }
 }
