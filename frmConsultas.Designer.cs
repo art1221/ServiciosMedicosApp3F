088705c49df80cbaa29e5b953cf6c76c6f04749e
art1221 <arturo.rojas@utpuebla.edu.mx>
2026-07-17 13:47:51 -0600

    Version 17 de julio

--- frmConsultas.Designer.cs
+++ frmConsultas.Designer.cs
@@ -28,12 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbEstudiantes = new System.Windows.Forms.ComboBox();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.txtDiagnostico = new System.Windows.Forms.TextBox();
+            this.txtTratamiento = new System.Windows.Forms.TextBox();
+            this.chkJustificaFalta = new System.Windows.Forms.CheckBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.dgvConsultas = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbEstudiantes
+            // 
+            this.cmbEstudiantes.FormattingEnabled = true;
+            this.cmbEstudiantes.Location = new System.Drawing.Point(136, 21);
+            this.cmbEstudiantes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cmbEstudiantes.Name = "cmbEstudiantes";
+            this.cmbEstudiantes.Size = new System.Drawing.Size(216, 28);
+            this.cmbEstudiantes.TabIndex = 0;
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.Location = new System.Drawing.Point(136, 64);
+            this.dtpFecha.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(216, 26);
+            this.dtpFecha.TabIndex = 1;
+            // 
+            // txtDiagnostico
+            // 
+            this.txtDiagnostico.Location = new System.Drawing.Point(177, 102);
+            this.txtDiagnostico.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDiagnostico.Multiline = true;
+            this.txtDiagnostico.Name = "txtDiagnostico";
+            this.txtDiagnostico.Size = new System.Drawing.Size(294, 44);
+            this.txtDiagnostico.TabIndex = 2;
+            // 
+            // txtTratamiento
+            // 
+            this.txtTratamiento.Location = new System.Drawing.Point(177, 158);
+            this.txtTratamiento.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTratamiento.Multiline = true;
+            this.txtTratamiento.Name = "txtTratamiento";
+            this.txtTratamiento.Size = new System.Drawing.Size(294, 40);
+            this.txtTratamiento.TabIndex = 3;
+            // 
+            // chkJustificaFalta
+            // 
+            this.chkJustificaFalta.AutoSize = true;
+            this.chkJustificaFalta.Location = new System.Drawing.Point(177, 202);
+            this.chkJustificaFalta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.chkJustificaFalta.Name = "chkJustificaFalta";
+            this.chkJustificaFalta.Size = new System.Drawing.Size(42, 24);
+            this.chkJustificaFalta.TabIndex = 4;
+            this.chkJustificaFalta.Text = "Si";
+            this.chkJustificaFalta.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(475, 26);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(98, 85);
+            this.btnRegistrar.TabIndex = 5;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // dgvConsultas
+            // 
+            this.dgvConsultas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvConsultas.Location = new System.Drawing.Point(21, 238);
+            this.dgvConsultas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvConsultas.Name = "dgvConsultas";
+            this.dgvConsultas.Size = new System.Drawing.Size(557, 106);
+            this.dgvConsultas.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Estudiante:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Fecha:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 107);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Diagnostico:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 173);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Tratamiento:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 204);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Justifica Falta:";
+            // 
+            // frmConsultas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(626, 354);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvConsultas);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.chkJustificaFalta);
+            this.Controls.Add(this.txtTratamiento);
+            this.Controls.Add(this.txtDiagnostico);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.cmbEstudiantes);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmConsultas";
             this.Text = "frmConsultas";
+            this.Load += new System.EventHandler(this.frmConsultas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvConsultas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbEstudiantes;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.TextBox txtDiagnostico;
+        private System.Windows.Forms.TextBox txtTratamiento;
+        private System.Windows.Forms.CheckBox chkJustificaFalta;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.DataGridView dgvConsultas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
--- frmConsultas.cs
+++ frmConsultas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServiciosMedicosLib;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,63 @@ namespace ServiciosMedicosApp
         public frmConsultas()
         {
             InitializeComponent();
+        }
+
+        private void CargarEstudiantes()
+        {
+            cmbEstudiantes.Items.Clear();
+            var dao = new EstudianteDAO();
+            var lista = dao.ObtenerTodos();
+            foreach(var est in lista)
+            {
+                cmbEstudiantes.Items.Add(est);
+            }
+            cmbEstudiantes.DisplayMember = "Nombre";
+            cmbEstudiantes.ValueMember = "IdEstudiante";
+        }
+
+        private void frmConsultas_Load(object sender, EventArgs e)
+        {
+            dgvConsultas.Columns.Clear();
+            dgvConsultas.Columns.Add("Id", "ID");
+            dgvConsultas.Columns.Add("Estudiante", "Estudiante");
+            dgvConsultas.Columns.Add("Fecha", "Fecha");
+            dgvConsultas.Columns.Add("Diagnostico", "Diagnostico");
+            dgvConsultas.Columns.Add("Tratamiento", "Tratamiento");
+            dgvConsultas.Columns.Add("Justifica", "Justifica Falta");
+            CargarEstudiantes();
+            CargarConsultas();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if(cmbEstudiantes.SelectedItem == null)
+            {
+                MessageBox.Show("Seleccione un estudiante");
+                return;
+            }
+            var estudiante = (Estudiante)cmbEstudiantes.SelectedItem;
+            var consulta = new ConsultaMedica
+            {
+                IdEstudiante = estudiante.IdEstudiante,
+                Fecha = dtpFecha.Value,
+                Diagnostico = txtDiagnostico.Text.Trim(),
+                Tratamiento = txtTratamiento.Text.Trim(),
+                JustificaFalta = chkJustificaFalta.Checked
+            };
+            new ConsultaMedicaDAO().Insertar(consulta);
+            MessageBox.Show("Consulta Mèdica registrada");
+            CargarConsultas();
+        }
+        private void CargarConsultas()
+        {
+            dgvConsultas.Rows.Clear();
+            var dao = new ConsultaMedicaDAO();
+            var lista = dao.ObtenerTodasConEstudiantes();
+            foreach(var c in lista)
+            {
+                dgvConsultas.Rows.Add(c.IdConsulta,c.NombreEstudiante,c.Fecha.ToShortDateString(),c.Diagnostico,c.Tratamiento,c.JustificaFalta ? "Si":"No");
+            }
         }
     }
 }
